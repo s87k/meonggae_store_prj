38d73d593336cbba1b87e75eea576ae5a41a7930
KimByeongNyeon <qud5252@naver.com>
2024-06-12 12:11:49 +0900

    오류 해결

--- meonggae_prj/src/main/java/com/store/meonggae/user/contoller/MainController.java
+++ meonggae_prj/src/main/java/com/store/meonggae/user/contoller/MainController.java
@@ -2,7 +2,6 @@ package com.store.meonggae.user.contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
