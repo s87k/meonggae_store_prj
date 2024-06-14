a80219aa05bd68375be14a02c02165e947d5814d
choiyeon <2dpaesu@gmail.com>
2024-06-14 17:55:58 +0900

    카테고리 끝~!

--- meonggae_prj/src/main/java/com/store/meonggae/user/contoller/CategoryController.java
+++ meonggae_prj/src/main/java/com/store/meonggae/user/contoller/CategoryController.java
@@ -2,11 +2,13 @@ package com.store.meonggae.user.contoller;
 
 import java.util.List;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.store.meonggae.product.domain.CategoryDomain;
@@ -14,25 +16,46 @@ import com.store.meonggae.product.service.CategoryService;
 
 @Controller
 public class CategoryController {
-
-	/*
+	
 	@Autowired
 	private CategoryService categoryService;
 
-	@GetMapping("/index.do")
-	public String getParentCategory(Model model) {
-		List<CategoryDomain> parentCategory = categoryService.selectParentCategory();
-		model.addAttribute("parentCategory", parentCategory);
-		return "main_page/main_contents";
-	}
+	//전체보기 카테고리(대분류)
+		//@ResponseBody json형식으로 변환하여 클라이언트에게 전달.
+		@ResponseBody
+		@RequestMapping(value="/parentCategory.do", method=RequestMethod.GET, produces="application/json;charset=UTF-8")
+		public String parentCategory() {
+			List<CategoryDomain> parentCategory = categoryService.selectParentCategory();
+			
+			JSONArray jsonArr = new JSONArray();
+			for(CategoryDomain category : parentCategory) {
+				JSONObject jsonObj = new JSONObject();
+				jsonObj.put("categoryNum", category.getCategoryNum());
+				jsonObj.put("name", category.getName());
+				jsonObj.put("parentCategoryNum", category.getParentCategoryNum());
+				jsonArr.add(jsonObj);
+			}//for
+			return jsonArr.toJSONString();
+		}//parentCategory
+		
+		//전체보기 카테고리(소분류)
+		@ResponseBody
+		@RequestMapping(value="/subCategory.do", method=RequestMethod.GET, produces="application/json;charset=UTF-8")
+	    public String subCategory(@RequestParam("Category") String parentCategory) {
+			System.out.println("부모 = "+parentCategory);
+	        List<CategoryDomain> subCategory = categoryService.selectSubCategory(parentCategory);
+	        System.out.println("subCategory = "+subCategory);
+	        
+	        JSONArray jsonArr = new JSONArray();
+			for(CategoryDomain category : subCategory) {
+				JSONObject jsonObj = new JSONObject();
+				jsonObj.put("categoryNum", category.getCategoryNum());
+				jsonObj.put("name", category.getName());
+				jsonObj.put("parentCategoryNum", category.getParentCategoryNum());
+				jsonArr.add(jsonObj);
+			}//for
+	        
+	        return jsonArr.toJSONString();
+	    }//subCategory
 	
-	@GetMapping("/categories/{parentCategory}/subcategories")
-	@ResponseBody
-	public List<CategoryDomain> getSubCategory(@PathVariable("parentCategory") String parentCategory, Model model){
-		return categoryService.selectSubCategory(parentCategory);
-	}
-	*/
-	
-	
-	
-}
+}//class
