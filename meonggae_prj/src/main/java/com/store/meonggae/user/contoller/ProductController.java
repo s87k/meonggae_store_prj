649b5e856d601b68cff386a450c956611df8b421
KimByeongNyeon <qud5252@naver.com>
2024-06-18 15:45:37 +0900

    로그인 실행 전에 상품 등록 페이지 이동 막기

--- meonggae_prj/src/main/java/com/store/meonggae/user/contoller/ProductController.java
+++ meonggae_prj/src/main/java/com/store/meonggae/user/contoller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.store.meonggae.product.domain.ProductDomain;
 import com.store.meonggae.product.service.ProductAddService;
@@ -23,21 +24,22 @@ public class ProductController {
     private ProductAddService productAddService;
     
     @GetMapping("/product_page/product_add.do")
-    public String productAdd(Model model, HttpSession session) {
+    public String productAdd(Model model, HttpSession session, RedirectAttributes redirectAttributes) {
         // 사용자 정보를 세션에서 가져옴
         LoginDomain loginUser = (LoginDomain) session.getAttribute("user");
-        int memNum = loginUser.getMemNum();
-        System.out.println("유저 번호:"+memNum);
-        
+
         if (loginUser != null) {
+            int memNum = loginUser.getMemNum();
             List<ProductDomain> productList = productAddService.selectProductByUser(memNum);
             System.out.println(productList);
             model.addAttribute("productList", productList);
             model.addAttribute("user", loginUser);
             model.addAttribute("memNum", loginUser.getMemNum());
+            return "product_page/product_add";
+        } else {
+            redirectAttributes.addFlashAttribute("message", "로그인이 필요합니다.");
+            return "redirect:/index.do";
         }
-        
-        return "product_page/product_add";
     }
     
 	/*
