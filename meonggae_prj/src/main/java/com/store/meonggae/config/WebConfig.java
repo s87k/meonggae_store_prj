067cdd44e5e23b8cf59c4b25347c48268e97110a
KimByeongNyeon <qud5252@naver.com>
2024-06-18 15:48:03 +0900

    사용하지 않는 Interceptor 삭제

--- meonggae_prj/src/main/java/com/store/meonggae/config/WebConfig.java
+++ meonggae_prj/src/main/java/com/store/meonggae/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.store.meonggae.user.login.interceptor.LoginCheckInterceptor;
 import com.store.meonggae.user.login.interceptor.LoginInterCeptor;
 
 @Configuration
@@ -14,17 +13,20 @@ public class WebConfig implements WebMvcConfigurer {
 	@Autowired
 	private LoginInterCeptor userInterceptor;
 
-	@Autowired
-	private LoginCheckInterceptor loginCheckInterceptor;
+	/*
+	 * @Autowired private LoginCheckInterceptor loginCheckInterceptor;
+	 */
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(userInterceptor).order(1).addPathPatterns("/**")
 				.excludePathPatterns("${}/index.do/logout.do");
 
-		registry.addInterceptor(loginCheckInterceptor).order(0)
-				.addPathPatterns("/product_page/product_add.do", "/My/store/store_frm.do", "/meongae_talk/talk_page.do")
-				.excludePathPatterns("/index.do/logout.do");
+		/*
+		 * registry.addInterceptor(loginCheckInterceptor).order(0)
+		 * .addPathPatterns("/product_page/product_add.do", "/My/store/store_frm.do",
+		 * "/meongae_talk/talk_page.do") .excludePathPatterns("/index.do/logout.do");
+		 */
 	}
 
 }
