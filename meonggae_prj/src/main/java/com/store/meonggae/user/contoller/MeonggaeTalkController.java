eaf9f5a043e0529b946753b3f6c688ed045bb5c2
KimByeongNyeon <qud5252@naver.com>
2024-06-18 15:54:21 +0900

    톡 페이지 로그인 하기 전 접근 막기

--- meonggae_prj/src/main/java/com/store/meonggae/user/contoller/MeonggaeTalkController.java
+++ meonggae_prj/src/main/java/com/store/meonggae/user/contoller/MeonggaeTalkController.java
@@ -1,16 +1,31 @@
 package com.store.meonggae.user.contoller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.store.meonggae.product.domain.ProductDomain;
+import com.store.meonggae.user.login.domain.LoginDomain;
 
 @Controller
 public class MeonggaeTalkController {
 
 	@GetMapping("/meongae_talk/talk_page.do")
-	public String meongaeTalk() {
-		
+	public String meongaeTalk(Model model, HttpSession session, RedirectAttributes redirectAttributes) {
+		LoginDomain loginUser = (LoginDomain) session.getAttribute("user");
+
+        if (loginUser != null) {
+            model.addAttribute("user", loginUser);
 		return "meongae_talk/talk_page";
+	}else {
+        redirectAttributes.addFlashAttribute("message", "로그인이 필요합니다.");
+        return "redirect:/index.do";
+    }
+	
 	}
-	
-	
 }
