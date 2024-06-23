d01ba6b0c6871c89df51a79e83f9903ca4ba1b74
KimByeongNyeon <qud5252@naver.com>
2024-06-23 18:47:40 +0900

    상품 등록 초기 설정

--- meonggae_prj/src/main/java/com/store/meonggae/product/dao/ProductAddDAO.java
+++ meonggae_prj/src/main/java/com/store/meonggae/product/dao/ProductAddDAO.java
@@ -1,4 +1,3 @@
-
 package com.store.meonggae.product.dao;
 
 import java.util.List;
@@ -10,27 +9,34 @@ import org.springframework.stereotype.Component;
 
 import com.store.meonggae.dao.MybatisDAO;
 import com.store.meonggae.product.domain.ProductDomain;
-import com.store.meonggae.product.vo.ProductAddVO;
-import com.store.meonggae.user.login.domain.LoginDomain;
 
 @Component
 public class ProductAddDAO {
 
-	@Autowired
-	private MybatisDAO mbDAO;
+    @Autowired
+    private MybatisDAO mbDAO;
 
-	public List<ProductDomain> selectProductByUser(int memNum)throws PersistenceException {
-		try (SqlSession ss = mbDAO.getMyBatisHandler(false)) {
-			return ss.selectList("com.store.meonggae.product.selectProductByUser", memNum);
-		}
-	}// selectProductByUser
+    public List<ProductDomain> selectProductByUser(int memNum) throws PersistenceException {
+        try (SqlSession ss = mbDAO.getMyBatisHandler(false)) {
+            return ss.selectList("com.store.meonggae.product.selectProductByUser", memNum);
+        }
+    }
 
-	/*
-	 * public int inserctProduct(ProductAddVO padVO) { ProductDomain pd; try
-	 * (SqlSession ss = mbDAO.getMyBatisHandler(false)) { return
-	 * ss.insert("com.store.meonggae.product.inserctProduct"); } }// insertProduct
-	 * 
-	 * public int updateProudct(ProductAddVO padVO) { try (SqlSession ss =
-	 * mbDAO.getMyBatisHandler(false)) { return
-	 * ss.update("com.store.meonggae.product.updateProudct"); } } // updateProudct
-	 */}
+    public int insertProduct(ProductDomain product) {
+        try (SqlSession ss = mbDAO.getMyBatisHandler(true)) {
+            return ss.insert("com.store.meonggae.product.insertProduct", product);
+        }
+    }
+
+    public int insertProductImg(ProductDomain product) {
+        try (SqlSession ss = mbDAO.getMyBatisHandler(true)) {
+            return ss.insert("com.store.meonggae.product.insertProductImg", product);
+        }
+    }
+
+    public int updateProduct(ProductDomain product) {
+        try (SqlSession ss = mbDAO.getMyBatisHandler(true)) {
+            return ss.update("com.store.meonggae.product.updateProduct", product);
+        }
+    }
+}
--- meonggae_prj/src/main/java/com/store/meonggae/user/contoller/ProductController.java
+++ meonggae_prj/src/main/java/com/store/meonggae/user/contoller/ProductController.java
@@ -1,24 +1,29 @@
 package com.store.meonggae.user.contoller;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.store.meonggae.product.domain.ProductDomain;
 import com.store.meonggae.product.service.ProductAddService;
 import com.store.meonggae.user.login.domain.LoginDomain;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class ProductController {
-    
+
     @Autowired
     private ProductAddService productAddService;
-    
+
     @GetMapping("/product_page/product_add.do")
     public String productAdd(Model model, HttpSession session, RedirectAttributes redirectAttributes) {
         // 사용자 정보를 세션에서 가져옴
@@ -37,26 +42,43 @@ public class ProductController {
             return "redirect:/index.do";
         }
     }
-    
-	/*
-	 * @PostMapping("/product_page/product_add.do") public String
-	 * handleProductAdd(@RequestParam("productName") String productName,
-	 * 
-	 * @RequestParam("productPrice") String productPrice,
-	 * 
-	 * @RequestParam("productDescription") String productDescription, HttpSession
-	 * session, RedirectAttributes redirectAttributes) { // 사용자 정보를 세션에서 가져옴
-	 * LoginDomain loginUser = (LoginDomain) session.getAttribute("user");
-	 * 
-	 * if (loginUser != null) { // 상품 등록 로직 수행 ProductDomain product = new
-	 * ProductDomain(); product.setProductName(productName);
-	 * product.setProductPrice(productPrice);
-	 * product.setProductDescription(productDescription);
-	 * product.setUserMemNum(loginUser.getMemNum());
-	 * productAddService.addProduct(product);
-	 * 
-	 * return "redirect:/product_page/product_list.do"; } else {
-	 * redirectAttributes.addFlashAttribute("message", "로그인이 필요합니다."); return
-	 * "redirect:/index.do"; } }
-	 */
+
+    @PostMapping("/product_page/product_add.do")
+    public String handleProductAdd(@RequestParam("product-name") String productName,
+                                   @RequestParam("product-price") int productPrice,
+                                   @RequestParam("description") String productDescription,
+                                   @RequestParam("category") String category,
+                                   @RequestParam("condition") String condition,
+                                   @RequestParam("trade-addr") String tradeAddr,
+                                   @RequestParam("image") MultipartFile image,
+                                   HttpSession session,
+                                   RedirectAttributes redirectAttributes) {
+        // 사용자 정보를 세션에서 가져옴
+        LoginDomain loginUser = (LoginDomain) session.getAttribute("user");
+
+        if (loginUser != null) {
+            // 상품 등록 로직 수행
+            ProductDomain product = new ProductDomain();
+            product.setMemNum(loginUser.getMemNum());
+            product.setName(productName);
+            product.setPrice(productPrice);
+            product.setDetail(productDescription);
+            product.setCategory_num(category);
+            product.setQuality_code(condition);
+            product.setLocation(tradeAddr);
+
+            try {
+                productAddService.insertProduct(product, image);
+            } catch (IOException e) {
+                e.printStackTrace();
+                redirectAttributes.addFlashAttribute("message", "상품 등록 중 오류가 발생했습니다.");
+                return "redirect:/product_page/product_add.do";
+            }
+
+            return "redirect:/product_page/product_list.do";
+        } else {
+            redirectAttributes.addFlashAttribute("message", "로그인이 필요합니다.");
+            return "redirect:/index.do";
+        }
+    }
 }
